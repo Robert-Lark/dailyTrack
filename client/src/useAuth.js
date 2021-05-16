cc47bee82ffaec6f542c8ec8eae4e8cb41509c68
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-16 12:54:48 -0700

    completed first iteration of track a day app

---
+++ client/src/useAuth.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export default function useAuth(code) {
+  console.log("code 2", code)
+  const [accessToken, setAccessToken] = useState()
+  const [refreshToken, setRefreshToken] = useState()
+  const [expiresIn, setExpiresIn] = useState()
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/login", {
+        code,
+      })
+      .then(res => {
+        setAccessToken(res.data.accessToken)
+        setRefreshToken(res.data.refreshToken)
+        setExpiresIn(res.data.expiresIn)
+        //this removes all the url params from the users url string
+        //this is important as the token can sometimes show up in there
+        window.history.pushState({}, null, "/")
+        //this catch will redirect the user back to 
+        //the homepage if there is an error like their token has expired
+      })
+      .catch((err) => {
+        console.log(err)
+        //window.location = "/"
+      })
+  }, [code])
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
+        })
+        .then(res => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch((err) => {
+          console.log(err)
+          //window.location = "/"
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+  }, [refreshToken, expiresIn])
+
+  return accessToken
+}
